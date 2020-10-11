8998c903faaef584f74dbf4b9cbecefd00f91d64
maestro-007 <alinajjar.91@hotmail.com>
2020-10-11 13:48:55 +0300

    updated backup

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    public AudioClip boom;
+   
+    void OnCollisionEnter(Collision collisionInfo)
+     {
+         if(collisionInfo.collider.tag=="Obstacle")
+         {
+            movement.enabled = false;
+            AudioSource.PlayClipAtPoint(boom, transform.position,999);
+            FindObjectOfType<GameManager>().EndGame();
+        }
+     }
+
+}
